ec74cc179afe456ab55c820532811d62c8b10b48
weiyuhui <1264787656@qq.com>
2022-10-04 09:26:22 +0800

    初次创建项目

---
+++ ols-common/src/main/java/com/ols/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.ols.common.exception.user;
+
+import com.ols.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
